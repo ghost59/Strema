5e323fbda534292050ac6b6086068ddbcd1b79d9
Neil <as.1714554@gmail.com>
2026-05-29 11:21:37 -0400

    update for people

--- cards.py
+++ cards.py
@@ -1,34 +1,4 @@
-from data import Card, Cards, Deck, Enemy 
+from data import Card
 
-class Tether(Card):
-    def __init__(self):
-        pass
-    def card_types(self):
-        self.card_type = "Attacker"
-    def decrip(self):
-        self.description = "Jagged chains strip your enemies souls"
-    def cost(self):
-        self.mana_cost = 5
-        return self.mana_cost
-    def attack_powers(self):
-        self.attack_power = 10
-        return self.attack_power
-class Munch(Card):
-    def __init__(self):
-        self.size = 0
+munch = Card("Munch", 10, "attack", 10, "Munches down on the enemies", "grows after every kill",)
 
-    def card_types(self):
-        self.card_type = "Attacker"
-    def descrip(self):
-        self.description = "Munch, bite down on your enemies with the power of a god"
-    def cost(self):
-        self.mana_cost = 6
-        return self.mana_cost
-    def attack_powers(self):
-        self.attack_power = 12
-        return self.attack_power
-    def increae(self):
-        self.size += 10
-        
-
-    
--- data.py
+++ data.py
@@ -1,110 +1,46 @@
 import random
-from dataclasses import dataclass
-import sdl3
-from cards import Tether
-
-
-class Enemy: 
-    def __init__(self, attack, health, attack_power):
-        self.attack = attack
-        self.health = health
-        self.attack_power = attack_power
-        
-        self.mana = 10
-    def health_status(self):
-        return self.health
-    def Attacks(self):
-        num = random.randint(1, 3)
-        fire = 100
-        life = 200
-        evil = 30
-        picks = [fire,life,evil]
-
-        random.shuffle(picks)
-        chosen = random.choice(picks)
-        if chosen == fire:
-            self.mana -= 5
-        elif chosen == life:
-            self.mana -= 10
-        elif chosen == evil:
-            self.mana -= 10
-
-        return chosen
-
-
-        
-   
-
-class Player:
-    def __init__(self, attack: int, health: int, attack_power: int,):
-        self.attack = attack
-        self.health = health
-        self.attack_power = attack_power
-        
-        self.mana = 10
-        self.defense = 5
-    def health_status(self):
-        return self.health
-    def Attacks(self, pows):
-        pows = input("select your power")
-
-    
-        teth = Tether()
-        butt = 300
-        yesss = 20000
-        if pows == "tether":
-            self.mana -= teth.mana_cost
-            results = teth.attack_powers()
-        if pows == "butt":
-            self.mana -= 7
-            results = butt
-        if pows == "yesss":
-            self.mana -= 10
-            results = yesss
-        return results
-    def Defense(self):
-        choice = input("pick you power")
-        shield = 10
-        life = 15
-        if choice == shield:
-            self.mana -= 5
-            self.defense += shield
-        elif choice == life:
-            self.mana -= 10
-            self.defense += life
-        return self.defense
+from dataclasses import dataclass, field
+from typing import List
 
 @dataclass()
 class Card:
     name: str
     mana_cost: int
     card_type:str
-    x: int
-    y: int
-    h: int
-    w: int 
     attack_power: int
     description: str
-    effect: str
+    effect_type: str
+
 
 @dataclass()
 class Cards:
-    cards: list
-    
+    cards: List[Card] = field(default_factory=list)
 
 @dataclass()
 class Deck:
     deck: Cards
     hand: int
 
+@dataclass()
+class Enemy:
+    name: str 
+    power: int
+    weight: float
+    hand: Deck
+@dataclass()
+class Player:
+    name: str
+    power: int
+    mana: int
+    cards: List[Card] = field(default_factory=list)
+
+
+
+
 
     
 
-class card_form(Card):
-    def form(self):
-        rect = sdl3.SDL_FRect(self.x, self.y, self.h, self.w)
-        return rect
-    
+
 
 
 
